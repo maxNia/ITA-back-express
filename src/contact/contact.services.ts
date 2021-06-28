93da4419b25ca0fb0ef402057c7959423e1b92ec
MaxNia <nstratiy@gmail.com>
2021-06-28 18:06:06 +0300

    updated to Model queries

--- src/contact/contact.services.ts
+++ src/contact/contact.services.ts
@@ -1,15 +1,9 @@
-import {v4 as uuid} from 'uuid';
-import sequelize from '../config/database';
 import { IAddContact, IUpdateContact } from './contact.interfaces';
+import Contact from './contact.model';
 
 export async function addContact(body: IAddContact) {
   try {
-    const id = uuid();
-    const date: string = getDate();
-    
-    await sequelize.query(
-      `INSERT INTO contacts (uuid, name, email, createdAt, updatedAt) VALUES ('${id}', '${body.name}' , '${body.email}', '${date}', '${date}')`
-    );
+    await Contact.create(body);
   } catch (error: any) {
     console.error(error.message);
   }
@@ -17,21 +11,12 @@ export async function addContact(body: IAddContact) {
 
 export async function updateContactById(body: IUpdateContact, id: string) {
   try {
-    const date: string = getDate();
-
-    if (body.name) {
-      await sequelize.query(`UPDATE contacts SET name='${body.name}', updatedAt='${date}' WHERE uuid='${id}'`);
-    } else if (body.email) {
-      await sequelize.query(`UPDATE contacts SET email='${body.email}', updatedAt='${date}' WHERE uuid='${id}'`);
-    } else if (body.name && body.email) {
-      await sequelize.query(`UPDATE contacts SET name='${body.name}', email='${body.email}', updatedAt='${date}' WHERE uuid='${id}'`);
-    } 
+    await Contact.update(body, {
+      where: {
+        uuid: id,
+      }
+    });
   } catch(e: any) {
     console.error(e.message); 
   }
-}
-
-
-function getDate(): string {
-  return new Date().toISOString().replace('T', ' ').slice(0, 19);
 }
